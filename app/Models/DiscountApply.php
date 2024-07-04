8f60dbf62024c13da700f272e34732204d28dc17
NghiaDuong <101704565+astr0tas@users.noreply.github.com>
2024-07-04 14:50:51 +0700

    change intermediate tables from being models to pivots

--- app/Models/DiscountApply.php
+++ app/Models/DiscountApply.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class DiscountApply extends Model
+class DiscountApply extends Pivot
 {
     use HasFactory;
 
--- app/Models/FileOrderContain.php
+++ app/Models/FileOrderContain.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class FileOrderContain extends Model
+class FileOrderContain extends Pivot
 {
     use HasFactory;
 
