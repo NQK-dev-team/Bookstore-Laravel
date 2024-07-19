66493509b9226ffb0f7a98c1b3e265e9a86c0e67
astr0tas <nghia369ty@gmail.com>
2024-07-19 20:39:35 +0700

    use inline script instead separated js file because it was short af :)

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -338,5 +338,45 @@
 
 @section('postloads')
     @livewireScripts
-    <script src="{{ asset('assets/js/customer/home/script.js') }}"></script>
+    <script>
+        function checkOverflow(slideNavigateID, listID) {
+            const el = document.getElementById(listID);
+            const curOverflow = el.style.overflow;
+
+            if (!curOverflow || curOverflow === "visible")
+                el.style.overflow = "hidden";
+
+            const isOverflowing = el.clientWidth < el.scrollWidth ||
+                el.clientHeight < el.scrollHeight;
+
+            el.style.overflow = curOverflow;
+            if (isOverflowing) {
+                if (window.innerWidth >= 992)
+                    document.getElementById(slideNavigateID).style.display = 'flex';
+                else
+                    document.getElementById(slideNavigateID).style.display = 'none';
+            } else {
+                document.getElementById(slideNavigateID).style.display = 'none';
+            }
+        }
+
+        function addScrollEvent(navigateLeftID, navigateRightID, listID) {
+            document.getElementById(navigateRightID).addEventListener('click', function() {
+                document.getElementById(listID).scrollLeft += 500;
+            });
+
+            document.getElementById(navigateLeftID).addEventListener('click', function() {
+                document.getElementById(listID).scrollLeft -= 500;
+            });
+        }
+
+        function showHoveredBook(index) {
+            document.querySelectorAll(`div[name="bestSellerDetail"]:not(#bestSellerDetail_${ index })`).forEach(function(
+                div) {
+                div.style.display = 'none';
+            });
+
+            document.querySelector('#bestSellerDetail_' + index).style.display = 'block';
+        }
+    </script>
 @endsection
