c221f882d7de4cdcde9f1151621fa454e7122275
astr0tas <nghia369ty@gmail.com>
2024-08-05 21:55:25 +0700

    i have decided that im not  gonna do the admin home page and statistic page because  i'm kinda getting sick of it

--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -1,15 +1,18 @@
 @extends('components.layouts.admin')
 
 @section('preloads')
-    <title>
-        Welcome to Laravel
-    </title>
+    <title>I'm sorry, I was lazzy...</title>
 @endsection
 
 @section('page')
-    <h1>Haiiii</h1>
+    <div class='w-100 h-100 d-flex flex-column align-items-center justify-content-center p-5'>
+        <h1 class='text-center'>This page is not available due to the fact that the project creator (me) is too lazy on
+            thinking of an idea of a
+            good dashboard, so yeah...</h1>
+        <img src="https://i.pinimg.com/736x/6c/2a/3d/6c2a3d51345f6a0a4a668431d621b8f9.jpg" class='w-100 h-100'
+            style="max-width:500px; max-height:500px">
+    </div>
 @endsection
 
 @section('postloads')
-
 @endsection
